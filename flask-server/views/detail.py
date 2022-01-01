d3e8dd5f656bda010b15b1f7332d7af53c02570e
rigyeonghong <zxcvbee@naver.com>
2022-01-01 20:43:28 +0900

    jsonify 수정

--- flask-server/views/detail.py
+++ flask-server/views/detail.py
@@ -20,7 +20,7 @@ def detail(movie_idx):
         reviews = Review.query.filter((Review.movie_idx == movie_idx) & (Review.is_deleted == 0)).order_by(Review.review_date.desc()).all()
         print(reviews)
 
-        return movie_info, reviews
+        return jsonify(movie_info, reviews)
     
     # 댓글 추가 : 리뷰 테이블에 값 추가
     elif method == "POST":
