0de1247b66fbff0731c9475c3e03d764ff188511
hongui <honge7694@naver.com>
2022-01-03 23:23:00 +0900

    wishlist 수정

--- flask-server/views/wishlist.py
+++ flask-server/views/wishlist.py
@@ -8,18 +8,22 @@ bp = Blueprint("wishlist", __name__, url_prefix="/wishlist")
 def wishlist():
 
     user_idx = session['user_idx']
+    print(user_idx)
+    # user_idx = 1
 
     wish_list = db.session.query(Movie.movie_idx, Movie.movie_img_link, Movie.movie_title, Favorite.favorite_idx, Favorite.movie_idx, Favorite.user_idx, Favorite.favorite_date).filter(Movie.movie_idx == Favorite.movie_idx, Favorite.user_idx == user_idx).order_by(Favorite.favorite_date)
-    wish_data = {}
+    wish_data = []
     for wish in wish_list:
-        wish_data['wish_idx'] = wish.favorite_idx
-        wish_data['movie_idx'] = wish.movie_idx
-        wish_data['movie_title'] = wish.movie_title
-        wish_data['movie_img_link'] = wish.movie_img_link
-        wish_data['user_idx'] = wish.user_idx
-        wish_data['favorite_date'] = wish.favorite_date
+        wish_data.append({
+            'wish_idx': wish.favorite_idx,
+            'movie_idx': wish.movie_idx,
+            'movie_title': wish.movie_title,
+            'movie_img_link': wish.movie_img_link,
+            'user_idx': wish.user_idx,
+            'favorite_date': wish.favorite_date
+        })
 
+    wish_datas = dict(list(enumerate(wish_data)))
+    print(wish_datas)
 
-    # print(wish_data)
-
-    return jsonify(wish_data)
+    return jsonify(wish_datas)
