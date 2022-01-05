3e04b9c0e940851c6ea79811717c744947607a48
rigyeonghong <zxcvbee@naver.com>
2022-01-05 18:27:28 +0900

    email, nick 중복검사 api 분리

--- flask-server/views/auth.py
+++ flask-server/views/auth.py
@@ -5,13 +5,53 @@ from models.users import *
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
+
+@bp.route('/signup/id', methods=['POST'])
+def signup():
+    user = request.get_json()
+    user_id = user['email']
+
+    # user_id(email) 중복확인
+    same_user = User.query.filter(User.user_id == user_id).first()
+    
+    if same_user:
+        print(" 이미 존재하는 사용자입니다. ")
+        return {"result":"failed",
+                "content":"이미 존재하는 아이디입니다."}, 401
+
+    else:
+        print(" 아이디 중복 확인. ")
+        return jsonify({"result":"success",
+                        "content":"아이디 중복 확인"}, 200)
+
+
+@bp.route('/signup/nick', methods=['POST'])
+def nick():
+    user = request.get_json()
+    user_nick = user['nickname']
+
+    # nickname 중복확인
+    same_nick = User.query.filter(User.user_nick == user_nick).first()
+
+    if same_nick:
+        return ({
+            "result":"failed",
+            "content":"이미 존재하는 닉네임입니다."
+        }), 401
+    
+    else:
+        print(" 닉네임 중복 확인. ")
+        return jsonify({"result":"success",
+                        "content":"닉네임 중복 확인"}, 200)
+
+
 #회원가입 signup
 @bp.route('/signup', methods=['GET', 'POST'])
 def signup():
 
     # fe -> be json데이터 전달 받음.
     user = request.get_json()
-    
+
     if user != None :
         user_nick = user['nickname']
         user_id = user['email']
@@ -21,48 +61,31 @@ def signup():
         user_runningtime = user['runningtime']
         user_region = user['region']
         
-        # nickname이 db에 있는지 확인
-        same_nick = User.query.filter(User.user_nick == user_nick).first()
+        
+        #패스워드 암호화 해서 db에 저장
+        user_pw_hash = generate_password_hash(user_pw)
+        new_user = User(user_id, user_pw_hash, user_nick, user_number, user_genre, user_runningtime, user_region)
+        db.session.add(new_user)
+        db.session.commit()
 
-        if same_nick:
-            return ({
-                "result":"failed",
-                "content":"이미 존재하는 닉네임입니다."
-            }), 401
-        else:
-            # user 정보가 DB에 이미 있는지 확인
-            same_user = User.query.filter(User.user_id == user_id).first()
+        # db에서 생성된 user_idx 가져오기
+        user_saved = User.query.filter(User.user_id == user_id).first()
+        user_idx = user_saved.user_idx
 
-            # user 정보 없으면 패스워드 암호화 해서 저장
-            if not same_user:
-                user_pw_hash = generate_password_hash(user_pw)
-                new_user = User(user_id, user_pw_hash, user_nick, user_number, user_genre, user_runningtime, user_region)
-                db.session.add(new_user)
-                db.session.commit()
+        # user식별값 user_idx와 fe에서 띄울 usr_nick 
+        session['user_idx'] = user_idx
+        session['nickname'] = user_nick
 
-                # db에서 생성된 user_idx 가져오기
-                user_saved = User.query.filter(User.user_id == user_id).first()
-                user_idx = user_saved.user_idx
-
-                # user식별값 user_idx와 fe에서 띄울 usr_nick 
-                session['user_idx'] = user_idx
-                session['nickname'] = user_nick
-
-                print(" 가입이 완료되었습니다. ")
-                return jsonify({"result":"success",
-                                "content":"가입 성공!",
-                                "user_idx": user_idx}, 200)
-            else:
-                # 사용자 정보가 이미 있다면, 회원가입 페이지로
-                print(" 이미 존재하는 사용자입니다. ")
-                return {"result":"failed",
-                        "content":"이미 존재하는 아이디입니다."
-                        }, 401
+        print(" 가입이 완료되었습니다. ")
+        return jsonify({"result":"success",
+                        "content":"가입 성공!",
+                        "user_idx": user_idx}, 200)
+      
 
     return 'signup page'
 
 
-@bp.route('/signin', methods=['POST', 'GET'])
+@bp.route('/signin', methods=['GET', 'POST'])
 def login():
     user = request.get_json()
     
