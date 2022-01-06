fb222002abe2fd1da4bc8f192e14266aae8ab424
hongui <honge7694@naver.com>
2022-01-07 03:50:51 +0900

    score table 추가

---
+++ flask-server/models/score.py
@@ -0,0 +1,27 @@
+from db_connect import db
+
+class Score(db.Model):
+
+    __tablename__ = 'user_score_tb'
+
+    user_score_idx = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    movie_idx = db.Column(db.Integer, db.ForeignKey("movie_tb.movie_idx"), nullable=False)
+    user_idx = db.Column(db.Integer, db.ForeignKey("user_info_tb.user_idx"), nullable=False)
+    user_score_comedy = db.Column(db.String(50), default = 0)
+    user_score_fantasy  = db.Column(db.String(50), default = 0)
+    user_score_drama = db.Column(db.String(50), default = 0)
+    user_score_family = db.Column(db.String(50), default = 0)
+    user_score_mello = db.Column(db.String(50), default = 0)
+    user_score_social = db.Column(db.String(50), default = 0)
+    user_score_thriller = db.Column(db.String(50), default = 0)
+    user_score_exp = db.Column(db.String(50), default = 0)
+    user_score_horror = db.Column(db.String(50), default = 0)
+    user_score_human = db.Column(db.String(50), default = 0)
+    user_score_action = db.Column(db.String(50), default = 0)
+    user_score_sf = db.Column(db.String(50), default = 0)
+    user_score_history = db.Column(db.String(50), default = 0)
+    user_score_music = db.Column(db.String(50), default = 0)
+    user_score_homo = db.Column(db.String(50), default = 0)
+    user_score_adventure = db.Column(db.String(50), default = 0)
+    user_score_nature = db.Column(db.String(50), default = 0)
+
