52549bde582ca75e67348bd0b6c201cc9ca33405
Ubuntu <team13@elice-kdt-3rd-team-13.yp5icu4nmmtehemc11eyecqf3c.syx.internal.cloudapp.net>
2022-01-07 11:43:58 +0000

    WIP: oauth import path

--- flask-server/views/oauth.py
+++ flask-server/views/oauth.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, request, session, flash, redirect, url_for, g, jsonify
-import sys
-sys.path.append('../')
 from werkzeug.security import generate_password_hash, check_password_hash
 from models.users import *
-from key.kakao_client import kakao_client_id
+from .key.kakao_client import kakao_client_id
 import requests
 
 bp = Blueprint("kakao", __name__, url_prefix="/oauth/kakao")
